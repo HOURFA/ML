674175d7f68d39885b5a6f0fd0b0e86712388871
HOURFA <C109112113@NKUST.EDU.TW>
2024-06-19 09:48:15 +0800

    2024/06/19 (1)新增API註解 (2)preprocess_v1.1 :     (a)將正規化後的特徵進行量化 (2)feature_add_v1.1 :     (a)使用DecisionTreeClassifier_Model中load_data來載入特徵與目標     (b)添加限制預防將超出畫面的特徵加入資料集 (3)get_target_v2.0 :     (a)加入DEBUG flag 用來解決延遲所造成的角度旋轉問題     (b)新增移動判斷機制，使坦克能移動到可以射到對方的位置

--- TankMan-main/SourceCode/ml/model/features.py
+++ TankMan-main/SourceCode/ml/model/features.py
@@ -2,19 +2,33 @@ import os
 import pickle
 from ml.model.utils import *
 from ml.model.constant import *
-def preprocess(model, record = None, scene_info = None, object = None):     
+def preprocess(model, scene_info,record = None, object=None):
+    """
+    Preprocesses the input data based on the specified model.
+
+    Args:
+        model (str): The model to preprocess the data for. Possible values are "STRATEGY", "TANK", or "GUN".
+        record (list): A list to store the preprocessed features.
+        scene_info (dict): The scene information dictionary.
+        object (str, optional): The object to preprocess the data for. Defaults to None.
+
+    Returns:
+        list: The preprocessed features based on the specified model and object.
+              If the model is "STRATEGY" or "GUN", returns a list of features.
+              If the model is "TANK", returns a list of features and the object.
+
+    """
     x1, y1 = scene_info['x'], scene_info['y']        
     oil = scene_info['oil']
     bullet = scene_info['power']
     x2, y2 = None, None
     wall = None
     is_WALL, is_ENEMY = False, False
-    hit_target = False
-
+    feature = []
     if model == "STRATEGY":
         feature = [
-            oil / MAX_OIL,
-            bullet / MAX_BULLET
+            abs(round(oil / MAX_OIL, 3)),
+            abs(round(bullet / MAX_BULLET, 3))
         ]
         if record is not None:
             record.append(feature)
@@ -25,103 +39,204 @@ def preprocess(model, record = None, scene_info = None, object = None):
             is_ENEMY = True
         else:
             x2, y2 = get_nearest_resource(scene_info[object], x1, y1)
-        if object != "bullet_stations_info":
-            wall = get_closest_wall([x1, y1], [x2, y2], scene_info['walls_info'])
+        wall = get_closest_wall([x1 , y1], [x2 , y2], scene_info['walls_info'])        
         angle = scene_info['angle']
-        direction = get_direction(x1, y1, x2, y2)
-        is_WALL = True if wall is not None else False
-        object = wall if wall is not None else object
-        distance = wall['distance'] if wall is not None else get_distance([x1, y1], [x2, y2])
+        if wall is not None:
+            x2, y2 = wall['x'], wall['y']
+            is_WALL = True
+            object = wall
+            distance = wall['distance'] 
+        else:
+            is_WALL = False
+            object = object
+            distance = get_distance([x1, y1], [x2, y2])
+        direction = get_direction(x1, y1, x2, y2)            
         wallorenemy = 1 if (is_ENEMY or is_WALL) else 0
         feature = [
-            abs(x1 / WIDTH), 
-            abs(y1 / HEIGHT), 
-            abs(x2 / WIDTH),
-            abs(y2 / HEIGHT),
-            abs(angle / 360),
-            abs(direction / 8),
-            abs(distance / MAX_DISTANCE),
+            abs(round(x1 / WIDTH, 3)), 
+            abs(round(y1 / HEIGHT, 3)), 
+            abs(round(x2 / WIDTH, 3)),
+            abs(round(y2 / HEIGHT, 3)),
+            abs(round(angle / 360, 3)),
+            abs(round(direction / 8, 3)),
+            abs(round(distance / MAX_DISTANCE, 3)),
             wallorenemy
         ]
         if record is not None:
             record.append(feature)
         return feature, object 
-    elif model == "GUN":        
+    elif model == "GUN":
         if object == "competitor_info" or object == "bullet_stations_info" or object == "oil_stations_info":
             x2, y2 = scene_info[object][0]['x'], scene_info[object][0]['y']            
         else:
             x2, y2 = object['x'], object['y']
         angle = scene_info['gun_angle']
         direction = get_direction(x1, y1, x2, y2)
-        hit_target = will_hit_target({"x" : x1, "y" : y1}, angle,{"x" : x2, "y" : y2},RADIUS)
         feature = [
-            abs(angle / 360),
-            abs(direction / 8),
-            hit_target
-        ]            
+            abs(round(angle / 360, 3)),
+            abs(round(direction / 8, 3))
+        ]
         if record is not None:
             record.append(feature)
         return feature
-def feature_add(model_name, side, features, targets, DIR, feature,TANK_FRAME_COUNT, GUN_FRAME_COUNT, Diagonal):
-    if os.path.exists(DIR + f"/targets_{model_name}_{side}.pickle"):
-        with open(DIR + f"/targets_{model_name}_{side}.pickle", 'rb') as f:
-            targets = pickle.load(f)     
-    if os.path.exists(DIR + f"/features_{model_name}_{side}.pickle"):
-        with open(DIR + f"/features_{model_name}_{side}.pickle", 'rb') as f:
-            features = pickle.load(f)
+def feature_add(model_name, model, feature, FRAME_COUNT, Diagonal=None, DEBUG=None):
+    """
+    Add features to the given feature list based on the model name.
 
+    Args:
+        model_name (str): The name of the model.
+        model (object): The model object.
+        feature (list): The list of features to be added.
+        FRAME_COUNT (int): The number of frames.
+        Diagonal (bool, optional): The diagonal flag. Defaults to None.
+        DEBUG (bool, optional): The debug flag. Defaults to None.
+
+    Returns:
+        tuple: A tuple containing the Diagonal, and DEBUG values.
+    """
+    model.load_data()
     if model_name == "STRATEGY":
-        len = TANK_FRAME_COUNT
-        for fv in feature[:len]:
-            if int(fv[0]*100) < 50:
+        for fv in feature[:FRAME_COUNT]:
+            if int(fv[0] * 100) < 50:
                 target = FIND_OIL
-            elif int(fv[1]*10) <= 3:
+            elif int(fv[1] * 10) <= 3:
                 target = FIND_BULLET
             else:
                 target = FIND_ENEMY
-            features.append(fv)
-            targets.append(target)
+            model.features.append(fv)
+            model.targets.append(target)
     elif model_name == "TANK":
-        len = TANK_FRAME_COUNT   
-        for fv in feature[:len]:
-            angle = int(fv[4] * 360)
-            direction = int(fv[5] * 8)
-            disance = int(fv[6] * MAX_DISTANCE)
-            wallorenemy = fv[7]
-            target, _ = get_target(angle, direction, "TANK", Diagonal, disance, wallorenemy)
-            features.append(fv)
-            targets.append(target)
+        for fv in feature[:FRAME_COUNT]:
+            x1 = int(fv[0] * WIDTH)
+            y1 = int(fv[1] * HEIGHT)
+            x2 = int(fv[2] * WIDTH)
+            y2 = int(fv[3] * HEIGHT)
+            if abs(x1) < WIDTH and abs(y1) < HEIGHT and abs(x2) < WIDTH and abs(y2) < HEIGHT:
+                angle = int(fv[4] * 360)
+                direction = int(fv[5] * 8)
+                distance = int(fv[6] * MAX_DISTANCE)
+                wallorenemy = fv[7]
+                hit_target = will_hit_target({"x": x1, "y": y1}, angle, {"x": x2, "y": y2}, RADIUS) if wallorenemy == 1 else False
+                target, _, DEBUG = get_target(angle, direction, "TANK", distance = distance, wallorenemy = wallorenemy, hit_target = hit_target, source=[x1, y1], destination=[x2, y2], DEBUG=DEBUG)
+                model.features.append(fv)
+                model.targets.append(target)
     elif model_name == "GUN":
-        len = GUN_FRAME_COUNT        
-        for fv in feature[:len]:
+        for fv in feature[:FRAME_COUNT]:            
             angle = int(fv[0] * 360)
             direction = int(fv[1] * 8)
-            hit_target = fv[2]
-            target, Diagonal = get_target(angle, direction, "GUN", Diagonal, hit_target = hit_target)
-            features.append(fv)
-            targets.append(target)
-    print(f"-----{model_name} added {len} features-----")
-    return features, targets, Diagonal
+            target, Diagonal, DEBUG = get_target(angle, direction, "GUN", Diagonal=Diagonal, DEBUG=DEBUG)
+            model.features.append(fv)
+            model.targets.append(target)
+    print(f"-----{model_name} added {FRAME_COUNT} features-----")
+    return Diagonal, DEBUG
 
-def get_target(angle, direction, object, Diagonal=False, distance = MAX_DISTANCE, wallorenemy = 0, hit_target = False):
-    target = 0
+def get_target(angle, direction, object, Diagonal=False, distance=MAX_DISTANCE, wallorenemy=0, hit_target=False, source=None, destination=None, DEBUG=None):
+    """
+    Determines the target action based on the given parameters.
+
+    Args:
+        angle (float): The angle of the target.
+        direction (int): The direction of the target.
+        object (str): The object type ('TANK' or 'GUN').
+        Diagonal (bool, optional): Indicates if the target is diagonal. Defaults to False.
+        distance (float, optional): The distance to the target. Defaults to MAX_DISTANCE.
+        wallorenemy (int, optional): Indicates if the target is a wall or an enemy. Defaults to 0.
+        hit_target (bool, optional): Indicates if the target has been hit. Defaults to False.
+        source (tuple, optional): The source coordinates. Defaults to None.
+        destination (tuple, optional): The destination coordinates. Defaults to None.
+        DEBUG (bool, optional): Indicates if debug mode is enabled. Defaults to None.
+
+    Returns:
+        tuple: The target action, diagonal flag, and debug flag.
+    """
+    angle = angle % 360
     if object == "TANK":
-        if wallorenemy == 1 and distance <= RADIUS: 
-            return DONTMOVE, Diagonal
-    elif object == "GUN":
-        if hit_target:
-            return SHOOT, Diagonal
-    if angle < 0 : angle += 360
-    if angle == 360 : angle = 0
-
+        if wallorenemy == 1 and distance <= RADIUS:
+            if hit_target:
+                return DONTMOVE, Diagonal, DEBUG
+            else:
+                if direction in [ANGLE_0, ANGLE_180]:
+                    if angle == 90 or angle == 270:
+                        DEBUG = False
+                        if source[1] > destination[1]:
+                            target = FORWARD if angle == 270 else BACKWARD
+                        else:
+                            target = BACKWARD if angle == 270 else FORWARD
+                    else:
+                        if angle in [45, 225]:
+                            target = TURN_LEFT
+                            DEBUG = True
+                        elif angle in [0, 135, 180, 315]:
+                            target = TURN_RIGHT if not DEBUG else TURN_LEFT
+                elif direction in [ANGLE_90, ANGLE_270]:
+                    if angle == 0 or angle == 180:
+                        if source[0] < destination[0]:
+                            DEBUG = False
+                            target = FORWARD if angle == 0 else BACKWARD
+                        else:
+                            target = BACKWARD if angle == 0 else FORWARD
+                    else:
+                        if angle in [135, 315]:
+                            target = TURN_LEFT
+                            DEBUG = True
+                        elif angle in [45, 90, 225, 270]:
+                            target = TURN_RIGHT if not DEBUG else TURN_LEFT
+                elif direction in [ANGLE_45, ANGLE_225]:
+                    if angle == 135 or angle == 315:
+                        slope = (destination[1] - source[1]) / (destination[0] - source[0] + 0.0001)
+                        DEBUG = False
+                        if source[0] < destination[0]:
+                            if slope > 1:
+                                target = FORWARD if angle == 135 else BACKWARD
+                            else:
+                                target = BACKWARD if angle == 135 else FORWARD
+                        else:
+                            if slope < 1:
+                                target = FORWARD if angle == 135 else BACKWARD
+                            else:
+                                target = BACKWARD if angle == 135 else FORWARD
+                    else:
+                        if angle in [90, 270]:
+                            target = TURN_LEFT
+                            DEBUG = True
+                        elif angle in [0, 45, 180, 225]:
+                            target = TURN_RIGHT if not DEBUG else TURN_LEFT
+                elif direction in [ANGLE_135, ANGLE_315]:
+                    if angle == 45 or angle == 225:
+                        slope = (destination[1] - source[1]) / (destination[0] - source[0] + 0.0001)
+                        DEBUG = False
+                        if source[0] > destination[0]:
+                            if slope > -1:
+                                target = FORWARD if angle == 315 else BACKWARD
+                            else:
+                                target = BACKWARD if angle == 315 else FORWARD
+                        else:
+                            if slope < -1:
+                                target = FORWARD if angle == 315 else BACKWARD
+                            else:
+                                target = BACKWARD if angle == 315 else FORWARD
+                    else:
+                        if angle in [0, 135, 180, 315]:
+                            target = TURN_LEFT
+                            DEBUG = True
+                        elif angle in [90, 270]:
+                            target = TURN_RIGHT if not DEBUG else TURN_LEFT
+                cheack_hit_target_a = will_hit_target({"x": source[0], "y": source[1]}, angle + 90,
+                                                      {"x": destination[0], "y": destination[1]}, RADIUS)
+                cheack_hit_target_b = will_hit_target({"x": source[0], "y": source[1]}, angle - 90,
+                                                      {"x": destination[0], "y": destination[1]}, RADIUS)
+                target = DONTMOVE if (cheack_hit_target_a or cheack_hit_target_b) else target
+            return target, Diagonal, DEBUG
     if direction == ANGLE_0 or direction == ANGLE_180:
         if angle == 180:
+            DEBUG = False
             if object == "TANK":
                 target = FORWARD if direction == ANGLE_0 else BACKWARD
             elif object == "GUN":
                 target = SHOOT if direction == ANGLE_0 else AIM_LEFT
                 Diagonal = True if direction == ANGLE_180 else False
         elif angle == 0:
+            DEBUG = False
             if object == "TANK":
                 target = BACKWARD if direction == ANGLE_0 else FORWARD
             elif object == "GUN":
@@ -130,17 +245,22 @@ def get_target(angle, direction, object, Diagonal=False, distance = MAX_DISTANCE
         elif Diagonal and object == "GUN":
             target = AIM_LEFT
         elif angle in [45, 225]:
-                target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
+            target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
+            DEBUG = True
         elif angle in [90, 135, 270, 315]:
-                target = TURN_LEFT if object == "TANK" else AIM_LEFT
+            target = TURN_LEFT if object == "TANK" else AIM_LEFT
+            if DEBUG:
+                target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
     elif direction == ANGLE_90 or direction == ANGLE_270:
         if angle == 90:
+            DEBUG = False
             if object == "TANK":
                 target = BACKWARD if direction == ANGLE_90 else FORWARD
             elif object == "GUN":
                 target = SHOOT if direction == ANGLE_270 else AIM_LEFT
                 Diagonal = True if direction == ANGLE_90 else False
         elif angle == 270:
+            DEBUG = False
             if object == "TANK":
                 target = FORWARD if direction == ANGLE_90 else BACKWARD
             elif object == "GUN":
@@ -150,16 +270,21 @@ def get_target(angle, direction, object, Diagonal=False, distance = MAX_DISTANCE
             target = AIM_LEFT
         elif angle in [135, 315]:
             target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
+            DEBUG = True
         elif angle in [0, 45, 180, 225]:
             target = TURN_LEFT if object == "TANK" else AIM_LEFT
+            if DEBUG:
+                target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
     elif direction == ANGLE_45 or direction == ANGLE_225:
         if angle == 225:
+            DEBUG = False
             if object == "TANK":
                 target = FORWARD if direction == ANGLE_45 else BACKWARD
             elif object == "GUN":
                 target = SHOOT if direction == ANGLE_45 else AIM_LEFT
                 Diagonal = True if direction == ANGLE_225 else False
         elif angle == 45:
+            DEBUG = False
             if object == "TANK":
                 target = BACKWARD if direction == ANGLE_45 else FORWARD
             elif object == "GUN":
@@ -168,17 +293,22 @@ def get_target(angle, direction, object, Diagonal=False, distance = MAX_DISTANCE
         elif Diagonal and object == "GUN":
             target = AIM_LEFT
         elif angle in [90, 270]:
-            target = TURN_RIGHT if object == "TANK" else AIM_RIGHT                                 
+            target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
+            DEBUG = True
         elif angle in [0, 135, 180, 315]:
             target = TURN_LEFT if object == "TANK" else AIM_LEFT
+            if DEBUG:
+                target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
     elif direction == ANGLE_135 or direction == ANGLE_315:
         if angle == 315:
+            DEBUG = False
             if object == "TANK":
                 target = FORWARD if direction == ANGLE_135 else BACKWARD
             elif object == "GUN":
                 target = SHOOT if direction == ANGLE_135 else AIM_LEFT
                 Diagonal = True if direction == ANGLE_315 else False
         elif angle == 135:
+            DEBUG = False
             if object == "TANK":
                 target = BACKWARD if direction == ANGLE_135 else FORWARD
             elif object == "GUN":
@@ -187,7 +317,10 @@ def get_target(angle, direction, object, Diagonal=False, distance = MAX_DISTANCE
         elif Diagonal and object == "GUN":
             target = AIM_LEFT
         elif angle in [0, 180]:
-            target = TURN_RIGHT if object == "TANK" else AIM_RIGHT  
+            target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
+            DEBUG = True
         elif angle in [45, 90, 225, 270]:
             target = TURN_LEFT if object == "TANK" else AIM_LEFT
-    return target, Diagonal
+            if DEBUG:
+                target = TURN_RIGHT if object == "TANK" else AIM_RIGHT
+    return target, Diagonal, DEBUG
